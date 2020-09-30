5ad23e4aa2d56eee233a3b28ea7dc6d302b67ee7
Marta Kulig <bialka104b@interia.pl>
2020-09-30 21:35:22 +0200

    lesson 48

---
+++ Sekcja5_Vue-router_trasowanie_sciezek/48.Vue-router_straznicy-tras/main/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1>Witaj w vue</h1>
+    <ul>
+      <li>
+        <!-- tag="li"  ten atrybut zamieni domyślny znacznik router-link(<a>) na tag li-->
+        <!-- @click.native="test" nasłuchiwanie darzenia klik i wywołanie metody test -->
+        <router-link to="/" tag="li" @click.native="test"
+          >Strona glowna</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/about">Strona o nas</router-link>
+      </li>
+      <li>
+        <!-- każde kliknięcie na strona kontact przeniesie nas do strony o nas -->
+        <router-link to="/contact" @click.native="about"
+          >Strona kontakt</router-link
+        >
+      </li>
+    </ul>
+    <!-- komponet bedzie wyświetlał nam widoki(ich nazwy) do których przechodzimy -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    test() {
+      alert("alert z metody test");
+    },
+    about() {
+      this.$router.push("/about"); //każde kliknięcie na strona kontact przeniesie nas do strony o nas
+      //this.$router.replace('/about');//podobnie działa metoda replace, ale nie da się kliknąć potem wstecz w przeglądarce
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Sekcja5_Vue-router_trasowanie_sciezek/48.Vue-router_straznicy-tras/main/src/router/index.js
@@ -0,0 +1,63 @@
+import VueRouter from "vue-router";
+
+const Home = { template: "<div>Strona główna</div>" };
+const About = { template: "<div>Strona o nas</div>" };
+const Contact = { template: "<div>Strona kontakt</div>" };
+const UserGeneral = {
+  template: "<div> User General - informacje o uzytkowniku <router-view/></div>"
+};
+const User1 = {
+  template: "<div>Uzytkownik o ID {{$route.params.userId}}</div>"
+}; // w ten sposób przekazujemy id ze ścieżki do komponentu
+// uruchomienie apki pod tym adresem pokaże ID  http://localhost:8080/uzytkownik/1234
+const User2 = {
+  //w ten sposób pobieramy z właściwości naszego komponentu za pomoca props
+  props: ["userId"],
+  template: "<div>User o ID {{userId}}</div>"
+};
+
+const router = new VueRouter({
+  mode: "history", //spowoduje usunięcie hasha w ścieżce
+  routes: [
+    { path: "/",        component: Home,  meta: { tytul: 'Strona glowna' }},
+    { path: "/about",   component: About, meta: { tytul: "O nas"} },
+    { path: "/kontakt", redirect: "/contact" },// na tą ściezke będzie przekierowanie ze ściezki contact
+    //gdy ktoś wpisze http://localhost:8080/kontakt zostanie przekierowany na http://localhost:8080/contact
+    //możemy też określić alias w poniższej lini zamiast przekierowania, róźnica polega na tym że w ścieżke nie zostanie podmieniona nazwa tylko zostanie taka jaką podał uzytkownik
+    { path: "/contact", alias:'/kontakt', component: Contact },
+    {
+      path: "/usersGeneral", //http://localhost:8080/usersGeneral
+      component: UserGeneral,
+      children: [
+        //dziecko tej ścieżki
+        {
+          path: "profile/:userId", //http://localhost:8080/usersGeneral/profile/1234
+          component: User2,
+          props: true
+        }
+      ]
+    },
+    {
+      // w ten sposób przekazujemy id ze ścieżki do komponentu
+      path: "/uzytkownik/:userId",
+      component: User1
+    },
+    {
+      //w ten sposób pobieramy z właściwości naszego komponentu za pomoca props
+      path: "/user/:userId",
+      component: User2,
+      props: true
+    }
+  ]
+});
+
+// router.beforeEach((to, from, next)=> {
+
+// })
+
+// (to)=> gdzie chcemy być przekierowani
+router.afterEach((to)=>{//powoduje to aktualizacje tagu title w head
+  document.title = to.meta.tytul
+})
+
+export default router;
