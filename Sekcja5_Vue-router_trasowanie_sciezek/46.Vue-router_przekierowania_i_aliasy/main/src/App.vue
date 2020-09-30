81c33b2318566ad4001435722a20cd4249b58683
Marta Kulig <bialka104b@interia.pl>
2020-09-30 19:50:45 +0200

    lesson 46

---
+++ Sekcja5_Vue-router_trasowanie_sciezek/46.Vue-router_przekierowania_i_aliasy/main/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Witaj w vue</h1>
+    <ul>
+      <li>
+        <router-link to="/">Strona glowna</router-link>
+      </li>
+      <li>
+        <router-link to="/about">Strona o nas</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Strona kontakt</router-link>
+      </li>
+    </ul>
+    <!-- komponet bedzie wyświetlał nam widoki(ich nazwy) do których przechodzimy -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
