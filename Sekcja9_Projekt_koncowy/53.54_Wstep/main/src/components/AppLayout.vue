5896820acda30d309b85c31c3c4918af7a01c8ba
Marta Kulig <bialka104b@interia.pl>
2020-10-02 12:38:29 +0200

    Stage 57. adds AppLayout.vue and router.js, usage stylus

---
+++ Sekcja9_Projekt_koncowy/53.54_Wstep/main/src/components/AppLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-layout">
+    <header class="header">
+      <div>
+        <img src="../assets/logo.svg" alt="" class="img" />
+        <div>Centrum wsparcia dla firm</div>
+      </div>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="stylus">
+@import '../style/style/main.styl';
+</style>
+
+// chcemy żeby te style były tylko dla tego komponentu "scoped"
+<style lang="stylus" scoped>
+.header{
+    .img {
+        width: 64px;
+        height:64px;
+    }
+}
+</style>
--- Sekcja9_Projekt_koncowy/53.54_Wstep/main/src/main.js
+++ Sekcja9_Projekt_koncowy/53.54_Wstep/main/src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-// import App from './App'
-
 import 'babel-polyfill';
-import Vue from 'vue'
+import Vue from 'vue';
+import AppLayout from './components/AppLayout';//import komponentu App layout
 
 new Vue({
   el: "#app",
   render: (h)=> {
-    return h('div', 'centrum wsparcia')
+    // return h('div', 'centrum wsparcia')
+    return h(AppLayout);
   }
 })
 
